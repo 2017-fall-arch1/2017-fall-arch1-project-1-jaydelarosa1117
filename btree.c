d752c0d9e1328614af85a44d654bf30d19b30b2f
jaydelarosa1117 <jaydelarosa111@yahoo.com>
2017-10-16 00:46:28 -0600

    functioning system with a few bugs

--- btree.c
+++ btree.c
@@ -5,9 +5,9 @@
 
 node *insert(node* root,char *newName){
   if(root == NULL){
-    char *name = malloc(sizeof(newName));
+    char *name = malloc(255);
     strcpy(name,newName);
-    root = (node*)malloc(sizeof(node));
+    root = (node*)malloc(255);
     root->name = name;
     return root;
   }
@@ -26,7 +26,7 @@ node *removeNode(node* root, char *newName){
   if(root == NULL){
     return root;
   }
-  else if(~strcmp(newName,root->name)){
+  else if(strcmp(newName,root->name)==0){
     if(root->leftNode == NULL && root->rightNode == NULL){
       free(root);
       return NULL;
@@ -99,11 +99,11 @@ void treeToFile(node* root, char* fileName){
 node *fileToTree(node* root, char *fileName){
   FILE *file = fopen(fileName, "r"); 
   if(file!=NULL){
-    char line[100];
-    char name[100];
+    char line[255];
+    char name[255];
     int i = 1;
     while(fgets(line,255,file)!=NULL){
-      sscanf(line,"%s", name);
+      sscanf(line,"%[^\t\n]s", name);
       root = insert(root,name);
       i++;
     }
@@ -118,48 +118,59 @@ void ui(){
   printf("Welcome to the employee manager\nWhat would you like to do\n");
   int choice = -1;
   node *employees = NULL;
-  while(choice != 0){
+  while(1){
     printf("0 quit\n1 print employees\n2 add employee\n3 delete employee\n");
     printf("4 read from file\n5 store on file\n");
     scanf("%d",&choice);
-
-    if(choice == 1){
+    
+    if(choice == 0){
+      printf("\nsave before quitting?\n0 no\n1 yes");
+      scanf("%d", &choice);
+      if(choice == 0){
+	return;
+      }
+      else if(choice == 1){
+	char fileName[255];
+	printf("enter file to write to\n");
+	scanf("%s",fileName);
+	treeToFile(employees,fileName);
+      }
+    }
+    else if(choice == 1){
       printf("\n");
       printTree(employees);
       printf("\n");
     }
     else if(choice == 2){
-      char newName[150];
+      char newName[255];
       printf("enter new employee name\n");
-      scanf("%[^\t\n]s",newName);
+      scanf(" %[^\t\n]s",&newName);
       employees = insert(employees, newName);
     }
     else if(choice == 3){
-      char name[150];
+      char name[255];
       printf("enter employee to remove\n");
-      scanf("%s",name);
-      removeNode(employees,name);
+      scanf(" %[^\t\n]s",&name);
+      employees = removeNode(employees,name);
     }
     else if(choice == 4){
-      char fileName[150];
+      char fileName[255];
       printf("enter file to read from\n");
       scanf("%s",fileName);
       employees = fileToTree(employees,fileName);
     }
     else if(choice == 5){
-      char fileName[150];
+      char fileName[255];
       printf("enter file to write to\n");
       scanf("%s",fileName);
       treeToFile(employees,fileName);
     }
+    
   }
 }
 
 int main(){
-  //ui();
-  node *n;// = (node*)malloc(sizeof(node));
-  n = insert(n,"thing of dreams");
-  printTree(n);
+  ui();
   return 0;
 }
 
