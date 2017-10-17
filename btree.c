ea1f6dcc8c88efde5f518a69234b8bff32479993
jaydelarosa1117 <jaydelarosa111@yahoo.com>
2017-10-17 15:55:51 -0600

    commented code

--- btree.c
+++ btree.c
@@ -4,7 +4,9 @@
 #include <string.h>
 #include <ctype.h>
 
+//recieves btree as parameter and prints tree in ascending order
 void printTree(node* root){
+  //if node or value is null return withouth printing
   if(root == NULL||root->name==NULL){
     return;
   }
@@ -13,19 +15,21 @@ void printTree(node* root){
   printTree(root->rightNode);
 }
 
-node *insert(node* root,char *newName){     
+// recieves tree and string as parameter and inserts it into the tree in an orderly manner
+node *insert(node* root,char *newName){
+  //when reaching an empty node assign it the string value and return the node
   if(root == NULL||root->name==NULL){
     char *name = malloc(255);
-    strcpy(name,newName);
-    root = (node*)malloc(255);
-    root->name = name;
+    strcpy(name,newName); // create new variable and copy value from original to remove the pointer 
+    root = (node*)malloc(255); //allocate memory for new node
+    root->name = name; //assign name and return node
     return root;
   }
-  else if(strcmp(newName,root->name)==-1){
+  else if(strcmp(newName,root->name)==-1){ // if string is before the current node value then go left
      root->leftNode = insert(root->leftNode,newName);
    }
   
-  else if(strcmp(newName, root->name)){
+  else if(strcmp(newName, root->name)){ // if string is after the current node value then go right
     root->rightNode = insert(root->rightNode,newName);
   }
   
@@ -33,46 +37,47 @@ node *insert(node* root,char *newName){
 }  
 
 node *removeNode(node* root, char *newName){
-  if(root == NULL){
+  if(root == NULL){//if the node is null return the user was not found
     return root;
   }
   else if(strcmp(newName,root->name)==0){
-    if(root->leftNode == NULL && root->rightNode == NULL){
+    if(root->leftNode == NULL && root->rightNode == NULL){//if value  matches current node and no children free allocated memory and return null
       free(root);
       return NULL;
     }
-    else if(root->rightNode == NULL){
-      char *mx = maxNode(root->leftNode);
-      root->name = mx;
-      root->leftNode =  removeNode(root->leftNode,mx);
+    else if(root->rightNode == NULL){//if only has left node replace current value with max of left node and remove max of left node
+      char *mx = maxNode(root->leftNode);//find max value and return
+      root->name = mx; //assign max value to current node
+      root->leftNode =  removeNode(root->leftNode,mx); //delete max value
     }
-    else if(root->leftNode == NULL){
-      char *mn = minNode(root->rightNode);
-      root->name = mn;
-      root->rightNode =  removeNode(root->rightNode,mn);
-    }
-    else{
+    else if(root->leftNode == NULL){//if only has right node replace current value with min of right node and remove min of right node
+	char *mn = minNode(root->rightNode);// find min value and return
+	root->name = mn; // assign min value to current node
+	root->rightNode =  removeNode(root->rightNode,mn); //remove min value
+      }
+    else{//same as above in the case that it has both children
       char *mn = minNode(root->rightNode);
       root->name = mn;
       root->rightNode =  removeNode(root->rightNode,mn);
     }
   }
-  else if(strcmp(newName,root->name)==-1){
+  else if(strcmp(newName,root->name)==-1){//if before than current node search on the left side 
     root->leftNode = removeNode(root->leftNode,newName);
   }
-  else if(strcmp(newName,root->name)){
+  else if(strcmp(newName,root->name)){//if after than current node search on right side
     root->rightNode = removeNode(root->rightNode,newName);
   }
   return root;
 }
 
-
+  //find max value in tree and return
 char* maxNode(node* root){
   if(root->rightNode == NULL){
     return root->name;
   }
   return maxNode(root->rightNode);
 }
+  //find min value in tree and return
 char* minNode(node* root){
   if(root->leftNode == NULL){
     return root->name;
@@ -80,38 +85,43 @@ char* minNode(node* root){
   return minNode(root->leftNode);
 }
 
+//recursivly prints writes tree to file
 void writerHelper(node* root, FILE *file){
   if(root==NULL || root->name==NULL){
     return;
   }
-  printf("%s\n",root->name);
-  fprintf(file,"%s\n",root->name);
-  writerHelper(root->leftNode,file);
-  writerHelper(root->rightNode,file);
+  fprintf(file,"%s\n",root->name);//writes to file
+  writerHelper(root->leftNode,file);//goes to left node
+  writerHelper(root->rightNode,file);//goes to right node
 }
+//main file writer, opens file and calls helper method
 void treeToFile(node* root, char* fileName){
   FILE *file = fopen(fileName,"w");
-  writerHelper(root,file);
+  if(file){
+    writerHelper(root,file);
+    fclose(file);
+  }
 }
-
+//reads file and stores into tree
 node *fileToTree(node* root, char *fileName){
   FILE *file = fopen(fileName,"r");
-  if(!file){
-    return root;
-  }
-  char c;
-  char *str = malloc(255);
-  int i = 0;
-  for(c = fgetc(file);c != EOF;c = fgetc(file)){
-    if(c != '\n'){
-      str[i] = c;
-      i++;
+  if(file){
+    char c;
+    char *str = malloc(255);
+    int i = 0;
+    //checks file until EOF marker
+    for(c = fgetc(file);c != EOF;c = fgetc(file)){
+      if(c != '\n'){//if read character isnt new line
+	str[i] = c;
+	i++;
+      }
+      else{//if it is replace with a 0 and insert to tree
+	str[i] = 0;
+	root = insert(root,str);
+	i = 0;
+      }
     }
-    else{
-      str[i] = 0;
-      root = insert(root,str);
-      i = 0;
-    }
+    fclose(file);
   }
   return root;
 }
--- btree.h
+++ btree.h
@@ -1,18 +1,25 @@
 #ifndef btree_include
 #define btree_include
+//new struct that is used to create a node type that can point to other nodes and contains a string
 typedef struct node{
   char* name;
   struct node *leftNode;
   struct node *rightNode;
 } node;
-
+//header for printing tree
 void printTree(node* root);
+//header for inserting to tree
 struct node *insert(node* root, char *newNam);
+//header to remove given element from tree
 struct node *removeNode(node* root, char *newName);
+//header for returning the rightmost element
 char *maxNode(node* root);
+//header for returning the leftmost element
 char *minNode(node* root);
-char *rmSpec(char* newName);
+//header to read file and turn into binary tree
 struct node *fileToTree(node* root, char *fileName);
+//writes contents from the tree onto a file depth first
 void treeToFile(node* root, char *fileName);
+//helper method for writing
 void writerHelper(node* root, FILE *file);
 #endif
